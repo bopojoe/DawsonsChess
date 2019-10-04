68afd387cb50a0b5359d3939456157587698c9f5
20074556 <20074556@it103imac13.wit.ie>
2019-10-04 11:57:52 +0100

    Initial Commit

---
+++ DawsonsChess/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  DawsonsChess
+//
+//  Created by 20074556 on 04/10/2019.
+//  Copyright © 2019 20074556. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
